783308deffeaf4d73ed7f084296828c85102e184
solipsse <solipsse@users.noreply.github.com>
2024-07-04 03:33:58 +0000

    deploy: 137ce3b28330ac87b6aeffa90babf0ed9641c255

---
+++ _static/analytic.js
@@ -0,0 +1,10 @@
+(function() {
+  var script = document.createElement('script');
+  script.async = true;
+  script.src = 'https://www.googletagmanager.com/gtag/js?id=G-LYW22V2HG4';
+  document.head.appendChild(script);
+})();
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+
+gtag('config', 'G-LYW22V2HG4');
